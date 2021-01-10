b3dec1ca827aba1f730dc4b1cf98fe13235124b4
timothy102 <cvetko.tim@gmail.com>
2021-01-10 13:00:49 +0100

    Added cache

--- styletransfer.py
+++ styletransfer.py
@@ -13,6 +13,7 @@ import tensorflow as tf
 import tensorflow_hub as hub
 print("Eager mode enabled: ", tf.executing_eagerly())
 
+@st.cache(surpress_st_warning=True)
 def crop_center(image):
   """Returns a cropped square image."""
   shape = image.shape
@@ -24,12 +25,10 @@ def crop_center(image):
   return image
 
 @functools.lru_cache(maxsize=10000)
-@st.cache(allow_output_mutation=True, max_entries=10, ttl=3600)
+@st.cache(allow_output_mutation=True, max_entries=1000, ttl=3600,surpress_st_warning=True)
 def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
   """Loads and preprocesses images."""
-  # Cache image file locally.
   image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
-  # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
   img = plt.imread(image_path).astype(np.float32)[np.newaxis, ...]
   if img.max() > 1.0:
     img = img / 255.
@@ -70,6 +69,7 @@ st.write('Neural Style Transfer is a technique that uses deep learning to compos
 
 st.write('NST algorithms are characterized by their use of deep neural networks for the sake of image transformation. This is implemented by optimizing the output image to match the content statistics of the content image and the style statistics of the style reference image. These statistics are extracted from the images using a convolutional network. Common uses for NST are the creation of artificial artwork from photographs, for example by transferring the appearance of famous paintings to user-supplied photographs. Several notable mobile apps use NST techniques for this purpose, including DeepArt and Prisma. This method has been used by artists and designers around the globe to develop new artwork based on existent style(s).')
 st.write("Feel free to play around with different styles and I strongly encourage you to try out the already made styles in the sidebar on your left. You'll be making transformations like the one below in a few seconds!")
+@st.cache(surpress_st_warning=True)
 def imageFromURL(url,img_shape=(320,240)):
     response = requests.get(url)
     image_bytes = io.BytesIO(response.content)
@@ -113,21 +113,21 @@ st.sidebar.write("Check the box above if you want to use your own style image. O
 content_image_url = st.text_input('Enter the content image URL here: ',content_image_url)
 style_image_url = st.text_input('Enter the style image URL here: ',style_image_url)
 
-content_image = load_image(content_image_url, content_img_size)
-style_image = load_image(style_image_url, style_img_size)
-style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
-
-outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
-stylized_image = outputs[0]
-
-# Visualize input images and the generated stylized image.
-
-stylized_image = tf.reshape(stylized_image,(output_image_size,output_image_size,3))
-stylized_image = np.array(stylized_image)
+@functools.lru_cache(maxsize=10000)
+@st.cache(allow_output_mutation=True, max_entries=1000, ttl=3600,surpress_st_warning=True)
+def prediction(content_image_url,style_image_url):
+  content_image = load_image(content_image_url, content_img_size) 
+  style_image = load_image(style_image_url, style_img_size) 
+  style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
+  outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+  stylized_image = outputs[0]
+  stylized_image = tf.reshape(stylized_image,(output_image_size,output_image_size,3))
+  stylized_image = np.array(stylized_image)
+  return stylized_image
 
 
 if st.button('Press this button to see your new image!'):
-  st.image(stylized_image)
+  st.image(prediction(content_image_url,style_image_url))
 ''' ## Further reading '''
 st.write("If you find neural style transfer fascinating and want to learn more, there are numerous number of resources you can use. As you read, don't get too caught up in the hard core of deep learning and mathematics and keep in mind that the same idea of applying styles is still essential.")
 st.write('Conceptually most closely related are methods using texture transfer to achieve artistic style transfer. However, these  approaches mainly rely on non-parametric techniques to directly manipulate the pixel representation of an image. In contrast, by using Deep Neural Networks trained on object recognition, we carry out manipulations in feature spaces that explicitly represent the high level content of an image.So this means that the specialty of the deep learning approach is to extract the style of an image not with mere pixel observation of the style picture, but rather the extracted features of the pre-trained model combined with the content of the style image. So, in essence, to discover the style of an image, we process the style image by analyzing its pixels feeding this information to the layer of a pre-trained model to “understand”/classify the provided input as objects. For a concrete walkthrough, there is a briliant article on [Sunhine at the moon](https://sunshineatnoon.github.io/posts/2017/05/blog-post-1/).')
